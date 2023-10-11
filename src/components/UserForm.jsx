cfb3ed6c5170fa5a2b6a5b415d2fbbca830027fc
German Cordova <german.sc937@gmail.com>
2023-10-11 00:25:09 -0500

    UpdateMain

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -3,91 +3,124 @@ import { InputText } from "primereact/inputtext";
 import { Password } from "primereact/password";
 import { Button } from "primereact/button";
 import { useState } from "react";
+import "../styles/userform.css";
 
 export function UserFormComponent() {
   const [value, setValue] = useState("");
 
   return (
     <>
-      <div
-        className="container h-full flex align-items-center justify-content-center"
-        style={{ border: "1px solid #000" }}
-      >
-        <div className="login w-4" style={{ border: "1px solid #000" }}>
-          <p>Ingresa</p>
+      <div className="container h-full flex align-items-center justify-content-center">
+        <div className="login w-4 p-3 m-2" style={{ border: "1px solid #ccc" }}>
+          <p className="text-center mt-2 mb-3">Ingresa</p>
           <div className="container-social-login">
-            <div className="facebook">
-              <i className="pi pi-facebook"></i>
+            <div className="facebook flex">
+              <i className="pi pi-facebook mr-2"></i>
               <Divider layout="vertical" />
-              <p> Continua con Facebook</p>
+              <p className="ml-2"> Continua con Facebook</p>
             </div>
-            <div className="google">
-              <i className="pi pi-google"></i>
+            <div className="google flex mt-2">
+              <i className="pi pi-google mr-2"></i>
               <Divider layout="vertical" />
-              <p> Continua con Google</p>
+              <p className="ml-2"> Continua con Google</p>
             </div>
           </div>
-          <p>o ingresas con tu correo electronico</p>
+          <p className="text-center mt-3 mb-3">
+            o ingresas con tu correo electronico
+          </p>
           <form action="">
-            <div className="flex flex-column gap-2">
+            <div className="flex flex-column gap-2 mb-3">
               <label htmlFor="email">Correo</label>
               <InputText
                 id="email"
                 aria-describedby="email-help"
                 keyfilter="email"
+                placeholder="Ingrese Correo"
               />
             </div>
             <div className="flex flex-column gap-2">
               <label htmlFor="password">Contraseña</label>
-              <Password
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-                feedback={false}
-                tabIndex={1}
-              />
-              <small>
+              <div>
+                <Password
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  feedback={false}
+                  tabIndex={1}
+                  placeholder="Contraseña"
+                  toggleMask
+                />
+              </div>
+              <small className="mb-3">
                 <a href="">¿Olvidaste tu contraseña?</a>
               </small>
             </div>
-            <Button label="Iniciar Sesion" severity="warning" />
+            <Button
+              label="Iniciar Sesion"
+              severity="warning"
+              className="btnLogin mb-3"
+            />
             <Divider />
-            <small>
+            <small className="mt-2">
               Al continuar, aceptas nuestros{" "}
               <a href="">Términos y condiciones.</a>
             </small>
           </form>
         </div>
-        <div className="register w-4" style={{ border: "1px solid #000" }}>
-          <p>¿Eres nuevo?</p>
+        <div
+          className="register w-4 p-3 m-2"
+          style={{ border: "1px solid #ccc" }}
+        >
+          <p className="mt-2 mb-3 text-center">¿Eres nuevo?</p>
           <form action="">
-            <div className="flex flex-column gap-2">
+            <div className="flex flex-column gap-2 mb-3">
               <label htmlFor="nombre">Nombre</label>
               <InputText id="nombre" placeholder="Ingrese nombres" />
             </div>
-
-            <div className="flex flex-column gap-2">
+            <div className="flex flex-column gap-2 mb-3">
               <label htmlFor="apellido">Apellido</label>
               <InputText id="apellido" placeholder="Ingrese apellidos" />
             </div>
-
-            <div className="flex flex-column gap-2">
+            <div className="flex flex-column gap-2 mb-3">
               <label htmlFor="email">Correo</label>
               <InputText
                 id="email"
                 aria-describedby="email-help"
                 keyfilter="email"
+                placeholder="Ingrese Correo"
               />
             </div>
-
-            <div className="flex flex-column gap-2">
+            <div className="flex flex-column gap-2 mb-3">
               <label htmlFor="password">Contraseña</label>
               <Password
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
                 feedback={false}
                 tabIndex={1}
+                placeholder="Contraseña"
               />
             </div>
+            <Button
+              label="Crear cuenta"
+              severity="warning"
+              className="btnRegister mb-3"
+            />
+            <div className="container-social-login mb-3">
+              <div className="facebook flex">
+                <i className="pi pi-facebook mr-2"></i>
+                <Divider layout="vertical" />
+                <p className="ml-2"> Continua con Facebook</p>
+              </div>
+              <div className="google flex mt-2">
+                <i className="pi pi-google mr-2"></i>
+                <Divider layout="vertical" />
+                <p className="ml-2"> Continua con Google</p>
+              </div>
+            </div>
+            <Divider />
+            <small className="mt-2">
+              Al continuar, aceptas nuestros{" "}
+              <a href="">Términos y condiciones.</a>
+            </small>
           </form>
         </div>
       </div>
--- src/pages/DestinosPage.jsx
+++ src/pages/DestinosPage.jsx
@@ -1,86 +1,42 @@
-import React from 'react';
-import '../styles/destino.css'; // Importa el archivo CSS
-export function Destinos(){
-    return(
-        
-        <>
-        <div className="contenedor">
-            <div id='contend' className="container fluid">
-                <div className="row">
-                    <div className="col-12">
-                        <h1>Destinos</h1>
-                        <h4>¡A recorrer el Perú! Descubre todo lo que puedes conocer en cada una de las regiones</h4>
-                    </div>
-                </div>
-            </div>
-            {/* ---------------------------------------- */}
-            <div className="container">
-                <h1>Lo más buscado por los viajeros</h1>
-                <div className="row">
-                    <div className="col-4">
+// import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+import { Image } from "primereact/image";
 
-                    </div>
-                    <div className="col-4">
-
-                    </div>
-                    <div className="col-4">
-
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-4">
-
-                    </div>
-                    <div className="col-4">
-
-                    </div>
-                    <div className="col-4">
-                        
-                    </div>
-                </div>
-            </div>
-            {/* ---------------------- */}
-
-            <div className="container">
-                <h1>Descubre tu país</h1>
-                <nav className="namvar">
-                    <li>
-                        <ul><a href="">Todos</a></ul>
-                        <ul><a href="">Norte</a></ul>
-                        <ul><a href="">Centro</a></ul>
-                        <ul><a href="">Sur</a></ul>
-                    </li>
-                </nav>
-                <div className="row">
-                    <div className="col-4">
-
-                    </div>
-                    <div className="col-4">
-
-                    </div>
-                    <div className="col-4">
-
-                    </div>
-                </div>
-            </div>
-            {/* ------------------------ */}
-
-            <div className="container">
-                <h1>Rutas Cortas</h1>
-                <div className="row">
-                    <div className="col-4">
-                        <p>Ver 12 lugares</p>
-                    </div>
-                    <div className="col-4">
-                        <p>Ver 6 lugares</p>
-                    </div>
-                    <div className="col-4">
-                        <p>Ver siete lugares</p>
-                    </div>
-                </div>
-            </div>
+import "../styles/destino.css"; // Importa el archivo CSS
+export function Destinos() {
+  return (
+    <>
+      <Navbar />
+      <div className="banner">
+        <div className="image-container-dest">
+          <Link to="/" className="nav-link enlace-dest mr-3 ml-3">
+            <i className="pi pi-angle-left"></i> Volver al inicio
+          </Link>
+          <Image
+            src="https://www.ytuqueplanes.com/imagenes/images/banners/destinos-laptop.jpg"
+            alt="Image"
+            height="380"
+            width="100%"
+          />
+          <div className="dest-cont-info">
+            <h2 className="dest-tit">Destinos</h2>
+            <p className="dest-p">
+              ¡A recorrer el Perú! Descubre todo lo que puedes conocer en cada
+              una de las regiones
+            </p>
+          </div>
         </div>
-        </>
-
-    );
+      </div>
+      <div className="mas-buscado">
+        <h2 className="mt-5 text-center text-bluegray-600">Lo más buscado por los viajeros</h2>
+      </div>
+      <div className="mas-buscado text-center">
+        <h2 className="mt-5 text-blue-700">Vive al mejor precio</h2>
+        <p>Aprovecha estas promociones para tu próximo viaje.</p>
+      </div>
+      <Footer />
+    </>
+  );
 }
